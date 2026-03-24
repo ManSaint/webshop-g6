9ae82bf06fdee3cbb1bc093321398896c4817421
fneel <70952301+fneel@users.noreply.github.com>
2026-03-24 23:21:37 +0100

    Added category card, categories grid and category page with dynamic product filtering (routing). Also added "?" to images in type: Product (types.ts). And changed next.config to accept all images with "*".

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import CategoryCard from "@/components/categories-card";
+import CategoriesGrid from "@/components/categories-grid";
 import ImageTextCTA from "@/components/ImageTextCTA";
+import data from "@/server/products.json";
+
+const categories = data.categories;
 
 export default async function Home() {
   return (
     <main>
       <ImageTextCTA />
+
+      {/*       {categories.map((category) => (
+        <CategoryCard key={category.id} category={category} />
+      ))} */}
+
+      <CategoriesGrid />
     </main>
   );
 }
